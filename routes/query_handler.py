cd9f88d5f6e2b3fdc9a32456972dfcc3d6b8c7b2
Ritesh-K-G <96720123+Ritesh-K-G@users.noreply.github.com>
2025-05-10 20:26:53 +0530

    Remove the asterix from response

--- routes/query_handler.py
+++ routes/query_handler.py
@@ -19,9 +19,10 @@ async def handle_query(payload: QueryInput):
                 "results": scenario_service.get_top_scenarios(query, history)
             }
         else:
+            query = "You are an AI Assistant specifically designed to provide accurate and relevant information regarding the Bhartiya Nyay Sanhita. Your task is to respond to the user query: " + query
             return {
                 "type": "general", 
-                "results": generate_gemini_response(query, history)
+                "results": generate_gemini_response(query, history).replace("*", " ").replace("`", " ")
             }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
